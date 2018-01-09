217e2d4d2e56aa93e7aaadc528f5491a908c12a2
Sergu <sergushov.nikita@gmail.com>
2018-01-09 16:48:54 +0300

    Update tds project Add correct *.tt files Set tds code generator

--- OutOfWebrotApp/Events/PostEvents.cs
+++ OutOfWebrotApp/Events/PostEvents.cs
@@ -15,24 +15,24 @@ namespace OutOfWebrotApp.Events
 	{
 		public void OnItemAdded(object sender, EventArgs args)
 		{
-			var addedItem = Event.ExtractParameter(args, 0) as Item;
-			var db = Sitecore.Configuration.Factory.GetDatabase(addedItem.Database.Name);
+			//var addedItem = Event.ExtractParameter(args, 0) as Item;
+			//var db = Sitecore.Configuration.Factory.GetDatabase(addedItem.Database.Name);
 
-			if (addedItem != null && db != null)
-			{
-				var emailService = new EmailService();
+			//if (addedItem != null && db != null)
+			//{
+			//	var emailService = new EmailService();
 
-				var getAllPostsItemsQuery = $"/sitecore/content/*[@@templatekey='{SitecoreHardcode.SiteTemplateName}']/Home/*[@@templatekey='{SitecoreHardcode.PostsItemTemplateName}']";
-				var postsItems = db.SelectItems(getAllPostsItemsQuery);
-				var isAddedItemIsPostItem = postsItems.Where(p => p.ID == addedItem.ParentID).ToList().Any();
+			//	var getAllPostsItemsQuery = $"/sitecore/content/*[@@templatekey='{SitecoreHardcode.SiteTemplateName}']/Home/*[@@templatekey='{SitecoreHardcode.PostsItemTemplateName}']";
+			//	var postsItems = db.SelectItems(getAllPostsItemsQuery);
+			//	var isAddedItemIsPostItem = postsItems.Where(p => p.ID == addedItem.ParentID).ToList().Any();
 
-				if (addedItem != null && isAddedItemIsPostItem)
-				{
-					var globalEmailConfig = SitecoreHelper.GetGlobalEmailSettingsItem();
-					var siteInfo = addedItem.GetSiteInfo();
-					emailService.SendCreatedPostByEmail(globalEmailConfig, siteInfo, addedItem);
-				}
-			}
+			//	if (addedItem != null && isAddedItemIsPostItem)
+			//	{
+			//		var globalEmailConfig = SitecoreHelper.GetGlobalEmailSettingsItem();
+			//		var siteInfo = addedItem.GetSiteInfo();
+			//		emailService.SendCreatedPostByEmail(globalEmailConfig, siteInfo, addedItem);
+			//	}
+			//}
 		}
 	}
 }
